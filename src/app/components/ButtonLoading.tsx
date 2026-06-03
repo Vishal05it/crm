5d78e1375d9eae9688f7ee13734ebf89ab553c67
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-03 11:14:29 +0530

    Loader color

--- src/app/components/ButtonLoading.tsx
+++ src/app/components/ButtonLoading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ButtonLoading() {
   return (
     <>
-      <div className="w-5 h-5 border-3 border-dashed rounded-full animate-spin border-indigo-600 dark:border-white"></div>
+      <div className="w-5 h-5 border-3 border-dashed rounded-full animate-spin border-sky-400 dark:border-white"></div>
     </>
   );
 }
